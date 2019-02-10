0aacb8ec1f1a8f57e92465f7b1ae97fc9327df83
Santiago Liñán Romero <s.linan10@uniandes.edu.co>
2019-02-10 00:40:26 -0500

    Visual changes

--- HowToGetDreamJob/Front2/apiCall.js
+++ HowToGetDreamJob/Front2/apiCall.js
@@ -1,33 +1,3 @@
-function openAlert() {
-    swal({
-        title: 'Select Outage Tier',
-        input: 'select',
-        inputOptions: {
-            '1': 'Tier 1',
-            '2': 'Tier 2',
-            '3': 'Tier 3'
-        },
-        inputPlaceholder: 'required',
-        showCancelButton: true,
-        inputValidator: function (value) {
-            return new Promise(function (resolve, reject) {
-                if (value !== '') {
-                    resolve();
-                } else {
-                    reject('You need to select a Tier');
-                }
-            });
-        }
-    }).then(function (result) {
-        if (result.value) {
-            swal({
-                type: 'success',
-                html: 'You selected: ' + result.value
-            });
-        }
-    });
-}
-
 function getJobs() {
     select = document.getElementById('select');
     $.get("http://ec2-3-92-47-95.compute-1.amazonaws.com:8080/api/jobs", function (data) {
@@ -54,84 +24,87 @@ function getPeople() {
     } else {
         $.get("http://ec2-3-92-47-95.compute-1.amazonaws.com:8080/api/paths?dbId=" + selected, function (data) {
             data.forEach(person => {
-                console.log(person)
-                var li = document.createElement('li');
-                li.classList.add("media");
-                li.classList.add("my-4");
-                var image = document.createElement('img');
-                image.src = person.image
-                image.width = 64
-                image.classList.add("rounded-circle");
-                image.classList.add("mr-3");
-                var div = document.createElement('div');
-                div.classList.add("media-body");
-                var h5 = document.createElement('h5');
-                h5.classList.add("mt-0");
-                h5.classList.add("mb-1");
-                h5.innerText = person.header
-                var p = document.createElement('p');
-                p.innerText = person.name
 
-                li.append(image)
-                li.append(div)
-                div.append(h5)
-                div.append(p)
-                people.append(li)
+                var mainDiv = document.createElement("div")
+                mainDiv.classList.add("card");
+                mainDiv.classList.add("border-info");
+                mainDiv.classList.add("big-card");
 
-                component = createPathComponent(person, select.options[select.selectedIndex].innerText);
-                document.getElementById("paths").append(component)
+                var body = document.createElement("div")
+                body.classList.add("card-body");
+                mainDiv.append(body);
+
+                var media = document.createElement("div")
+                media.classList.add("media");
+                body.append(media)
+
+                var img = document.createElement("img")
+                img.src = person.image
+                img.classList.add("mr-3");
+                img.width = 64
+                media.append(img)
+
+                var mediaBody = document.createElement("div")
+                mediaBody.classList.add("media-body");
+                media.append(mediaBody)
+
+                var heading = document.createElement("h5")
+                heading.innerText = person.header
+                heading.classList.add("mt-0");
+                mediaBody.append(heading)
+
+                var pName = document.createElement("p")
+                pName.innerText = person.name
+                mediaBody.append(pName)
+
+                var cardDeck = document.createElement("div")
+                cardDeck.classList.add("card-deck");
+                body.append(cardDeck)
+
+                jobs = person.jobs.reverse()
+
+                for (let i = 1; i < jobs.length + 1; i++) {
+                    const job = jobs[i - 1];
+                    var partsOfStr = job.split(':');
+
+                    var jobCard = document.createElement("div")
+                    jobCard.classList.add("card")
+                    jobCard.classList.add("mb-4")
+                    if (partsOfStr.includes(select.options[select.selectedIndex].innerText)) {
+                        jobCard.classList.add("bg-success")
+                        jobCard.classList.add("text-white")
+                    }
+                    cardDeck.append(jobCard)
+
+                    var cardHeader = document.createElement("div")
+                    cardHeader.innerText = i
+                    cardHeader.classList.add("card-header")
+                    jobCard.append(cardHeader)
+
+                    var cardBody = document.createElement("div")
+                    cardBody.classList.add("card-body")
+                    jobCard.append(cardBody)
+
+                    var jobName = document.createElement("h5")
+                    jobName.classList.add("card-title")
+                    jobName.innerText = partsOfStr[0]
+                    cardBody.append(jobName)
+
+                    if (partsOfStr[1]) {
+                        var text = document.createElement("p")
+                        text.classList.add("card-text")
+                        text.innerText=partsOfStr[1]
+                        cardBody.append(text)
+                    }
+                }
+                document.getElementById("paths").append(mainDiv);
+
             });
         });
-
         document.getElementById("part2").style.display = 'inline';
 
     }
 
 }
-
-function createPathComponent(person, jobName) {
-
-    jobs = person.jobs.reverse()
-
-    var div = document.createElement('div');
-    div.classList.add("col-4");
-    div.classList.add("list-group");
-
-    for (let i = 1; i < jobs.length + 1; i++) {
-        const job = jobs[i - 1];
-        console.log(job)
-        var partsOfStr = job.split(':');
-        var a = document.createElement('a');
-        a.classList.add("list-group-item");
-        a.classList.add("list-group-item-action");
-        if(partsOfStr.includes(jobName))
-        {
-            a.classList.add("active");
-        }
-        var div2 = document.createElement('div');
-        div2.classList.add("d-flex");
-        div2.classList.add("w-100");
-        div2.classList.add("justify-content-between");
-        var h5 = document.createElement('h5');
-        h5.classList.add("mb-1");
-        h5.innerText = i + ") " + partsOfStr[0]
-        div2.classList.add("list-group");
-        var p = document.createElement('p');
-        p.classList.add("mb-1");
-        if (partsOfStr[1]) {
-            p.innerHTML = partsOfStr[1]
-        }
-
-        div.append(a)
-        a.append(div2)
-        div2.append(h5)
-        a.append(p)
-    }
-
-    return div;
-
-
-}
-
 
 getJobs();
